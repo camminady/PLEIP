7c6ffb6cc1b0bb723bb942d279ef7d7142a857e0
Thomas <thomas@camminady.org>
2019-12-17 17:52:15 +0100

    CI setup step 2.

---
+++ pleip/__init__.py
@@ -0,0 +1,9 @@
+# pylint: disable=C0111
+
+from .core import distance_periodic, customnorm, distance_pointline
+
+__all__ = [
+    "distance_periodic",
+    "distance_pointline",
+    "customnorm"
+]
